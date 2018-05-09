2267109758c7da4f2cdc8b2a3206973bdb317cd9
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-09 18:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function sayHiToGrandma(string){
             return "I can't hear you!"
           } else if (string==="I love you, Grandma."){
                 return "I love you, too."
-              } 
-      
+              }
+
 }
